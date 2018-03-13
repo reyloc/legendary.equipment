e6bde86aa5da633b09daec3c1ce6f0f52e3d13f4
Jason Colyer <jcolyer2007@gmail.com>
2018-03-13 11:26:29 -0500

    Added all flaws

--- db/seeds/13_flaw.rb
+++ db/seeds/13_flaw.rb
@@ -16,5 +16,221 @@ Flaw.create([
   },
   {:description => "Once I pick a goal, I become obsessed with it to the detriment of everything else in my life.",
    :background_id => 1
-  }
+  },
+  {:description => "I can't resist a pretty face.",
+   :background_id => 2
+  },
+  {:description => "I'm always in debt. I spend my ill.gotten gains on decadent luxuries faster than I bring them in.",
+   :background_id => 2
+  },
+  {:description => "I'm convinced that no one could ever fool me the way I fool others.",
+   :background_id => 2
+  },
+  {:description => "I'm too greedy for my own good. I can't resist taking a risk if there's money involved.",
+   :background_id => 2
+  },
+  {:description => "I can't resist swindling people who are more powerful than me.",
+   :background_id => 2
+  },
+  {:description => "I hate to admit it and will hate myself for it, but I'll run and preserve my own hide if the going gets tough",
+   :background_id => 2
+  },
+  {:description => "When I see something valuable, I can't think about anything but how to steal it.",
+   :background_id => 3
+  },
+  {:description => "When faced with a choice between money and my friends, I usually choose the money.",
+   :background_id => 3
+  },
+  {:description => "If there's a plan, I'll forget it. If I don't forget it, I'll ignore it.",
+   :background_id => 3
+  },
+  {:description => "I have a 'tell' that reveals when I'm lying.",
+   :background_id => 3
+  },
+  {:description => "I turn tail and run when things look bad.",
+   :background_id => 3
+  },
+  {:description => "An innocent person is in prison for a crime that I committed. I'm okay with that.",
+   :background_id => 3
+  },
+  {:description => "I'll do anything lo win fame and renown.",
+   :background_id => 4
+  },
+  {:description => "I'm a sucker for a pretty face.",
+   :background_id => 4
+  },
+  {:description => "A scandal prevents me from ever going home again. That kind of trouble seems to follow me around.",
+   :background_id => 4
+  },
+  {:description => "I once satirized a noble who still wants my head. It was a mistake that I will likely repeat.",
+   :background_id => 4
+  },
+  {:description => "I have trouble keeping my true feelings hidden. My sharp tongue lands me in trouble.",
+   :background_id => 4
+  },
+  {:description => "Despite my best efforts, I am unreliable to my friends",
+   :background_id => 4
+  },
+  {:description => "The tyrant who rules my land will stop at nothing to see me killed.",
+   :background_id => 5
+  },
+  {:description => "I'm convinced of the significance of my destiny, and blind to my shortcomings and the risk of failure.",
+   :background_id => 5
+  },
+  {:description => "The people who knew me when I was young know my shameful secret, so I can never go home again.",
+   :background_id => 5
+  },
+  {:description => "I have a weakness for the vices of the city, especially hard drink.",
+   :background_id => 5
+  },
+  {:description => "Secretly, I believe that things would be better if I were a tyrant lording over the land.",
+   :background_id => 5
+  },
+  {:description => "I have trouble trusting in my allies.",
+   :background_id => 5
+  },
+  {:description => "I'll do anything to get my hands on something rare or priceless.",
+   :background_id => 7
+  },
+  {:description => "I'm quick to assume that someone is trying to cheat me.",
+   :background_id => 7
+  },
+  {:description => "No one must ever learn that I once stole money from guild coffers.",
+   :background_id => 7
+  },
+  {:description => "I'm never satisfied with what I have - I always want more.",
+   :background_id => 7
+  },
+  {:description => "I would kill to acquire a noble title.",
+   :background_id => 7
+  },
+  {:description => "I'm horribly jealous of anyone who can outshine my handiwork. Everywhere I go, I'm surrounded by rivals.",
+   :background_id => 7
+  },
+  {:description => "Now that I've returned to the world, I enjoy its delights a little too much.",
+   :background_id => 6
+  },
+  {:description => "I harbor dark, bloodthirsty thoughts that my isolation and meditation failed to quell.",
+   :background_id => 6
+  },
+  {:description => "I am dogmatic in my thoughts and philosophy.",
+   :background_id => 6
+  },
+  {:description => "I let my need to win arguments overshadow friendships and harmony.",
+   :background_id => 6
+  },
+  {:description => "I'd risk too much to uncover a lost bit of knowledge.",
+   :background_id => 6
+  },
+  {:description => "I like keeping secrets and won't share them with anyone.",
+   :background_id => 6
+  },
+  {:description => "I secretly believe that everyone is beneath me.",
+   :background_id => 8
+  },
+  {:description => "I hide a truly scandalous secret that would ruin my family forever.",
+   :background_id => 8
+  },
+  {:description => "I too often hear veiled insults and threats in every word addressed to me, and I'm quick to anger.",
+   :background_id => 8
+  },
+  {:description => "I have an insatiable desire for carnal pleasures.",
+   :background_id => 8
+  },
+  {:description => "In fact, the world does revolve around me.",
+   :background_id => 8
+  },
+  {:description => "By my words and actions, I often bring shame to my family.",
+   :background_id => 8
+  },
+  {:description => "I am too enamored of ale, wine, and other intoxicants.",
+   :background_id => 9
+  },
+  {:description => "There's no room for caution in a life lived to the fullest.",
+   :background_id => 9
+  },
+  {:description => "I remember every insult I've received and nurse a silent resentment toward anyone who's ever wronged me.",
+   :background_id => 9
+  },
+  {:description => "I am slow to trust members of other races, tribes, and societies.",
+   :background_id => 9
+  },
+  {:description => "Violence is my answer to almost any challenge.",
+   :background_id => 9
+  },
+  {:description => "Don't expect me to save those who can't save themselves. It is nature's way that the string thrive and the weak perish.",
+   :background_id => 9
+  },
+  {:description => "I am easily distracted by the promise of information.",
+   :background_id => 10
+  },
+  {:description => "Most people scream and run when they see a demon. I stop and take notes on its anatomy.",
+   :background_id => 10
+  },
+  {:description => "Unlocking an ancient mystery is worth the price of a civilization.",
+   :background_id => 10
+  },
+  {:description => "I overlook obvious solutions in favor of complicated ones.",
+   :background_id => 10
+  },
+  {:description => "I speak without really thinking through invariably insulting others.",
+   :background_id => 10
+  },
+  {:description => "I can't keep a secret to save my life, or anyone else's.",
+   :background_id => 10
+  },
+  {:description => "I follow orders, even if I think they're wrong.",
+   :background_id => 11
+  },
+  {:description => "I'll say anything lo avoid having to do extra work.",
+   :background_id => 11
+  },
+  {:description => "Once someone questions my courage, I never back down no matter how dangerous the situation.",
+   :background_id => 11
+  },
+  {:description => "Once I start drinking, it's hard for me to stop.",
+   :background_id => 11
+  },
+  {:description => "I can't help but pocket loose coins and other trinkets I come across.",
+   :background_id => 11
+  },
+  {:description => "My pride will probably lead lo my destruction.",
+   :background_id => 11
+  },
+  {:description => "The monstrous enemy we faced in battle still leaves me quivering with fear.",
+   :background_id => 12
+  },
+  {:description => "I have little respect for anyone who is not a proven warrior.",
+   :background_id => 12
+  },
+  {:description => "I made a terrible mistake in battle that cost many lives - and I would do anything to keep that mistake secret.",
+   :background_id => 12
+  },
+  {:description => "My hatred of my enemies is blind and unreasoning.",
+   :background_id => 12
+  },
+  {:description => "I obey the law, even if the law causes misery.",
+   :background_id => 12
+  },
+  {:description => "I'd rather eat my armor than admit when I'm wrong.",
+   :background_id => 12
+  },
+  {:description => "If I'm outnumbered, I will run away from a fight.",
+   :background_id => 13
+  },
+  {:description => "Gold seems like a lot of money to me, and I'll do just about anything for more of it.",
+   :background_id => 13
+  },
+  {:description => "I will never fully trust anyone other than myself.",
+   :background_id => 13
+  },
+  {:description => "I'd rather kill someone in their sleep then fight fair.",
+   :background_id => 13
+  },
+  {:description => "It's not stealing if I need It more than someone else.",
+   :background_id => 13
+  },
+  {:description => "People who can't take care of themselves get what they deserve.",
+   :background_id => 13
+  },
 ])

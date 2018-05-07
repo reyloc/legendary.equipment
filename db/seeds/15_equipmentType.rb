b45c48a480dac3680a8a79e07c9976ff94587bfe
Jason Colyer <jcolyer2007@gmail.com>
2018-05-07 10:46:34 -0500

    Added medium armor, heavy armor, shield

--- db/seeds/15_equipmentType.rb
+++ db/seeds/15_equipmentType.rb
@@ -7,5 +7,14 @@ EquipmentType.create([
   },
   {:name => 'Adventuring Gear',
    :description => 'Gear a traveling adventure might carry on their persons.'
+  },
+  {:name => 'Medium Armor',
+   :description => 'Medium armor offers more protection than light armor, but it also impairs movement more. If you wear medium armor, you add your Dexterity modifier, to a maximum of +2, to the base number from your armor type to determine your Armor Class.'
+  },
+  {:name => 'Heavy Armor',
+   :description => "Of all the armor categories, heavy armor offers the best protection. These suits of armor cover the entire body and are designed to stop a wide range of attacks. Only proficient warriors can manage their weight and bulk. Heavy armor doesn't let you add your Dexterity modifier to your Armor Class, but it also doesn't penalize you if your Dexterity modifier is negative."
+  },
+  {:name => 'Shield',
+   :description => "A shield is made from wood or metal and is carried in one hand. Wielding a shield increases your Armor Class by 2. You can benefit from only one shield at a time."
   }
 ])

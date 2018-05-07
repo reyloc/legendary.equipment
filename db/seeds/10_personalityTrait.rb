cf5dcfda2ee70d6cc5fc3d48ee90f267b9954039
Jason Colyer <jcolyer2007@gmail.com>
2018-05-07 10:45:45 -0500

    Added all background personality traits

--- db/seeds/10_personalityTrait.rb
+++ db/seeds/10_personalityTrait.rb
@@ -22,5 +22,287 @@ PersonalityTrait.create([
   },
   {:description => "I've spent so long in the temple that I have little practical experience in dealing with people in the outside world.",
    :background_id => 1
+  },
+  {:description => "I fall in and out of love easily, and am always pursuing someone.",
+   :background_id => 2
+  },
+  {:description => "I have a joke for every occasion, especially occasions where humor is inappropriate.",
+   :background_id => 2
+  },
+  {:description => "Flattery is my preferred trick for getting what I want.",
+   :background_id => 2
+  },
+  {:description => "I'm a born gambler who can't resist taking a risk for a potential payoff.",
+   :background_id => 2
+  },
+  {:description => "I lie about almost everything, even when there's no good reason to.",
+   :background_id => 2
+  },
+  {:description => "Sarcasm and insults are my weapons of choice.",
+   :background_id => 2
+  },
+  {:description => "I keep multiple holy symbols on me and invoke whichever deity might come in useful at any given moment.",
+   :background_id => 2
+  },
+  {:description => "I pocket anything I see that might have some value.",
+   :background_id => 2
+  },
+  {:description => "When I see something valuable, I can't think about anything but how to steal it.",
+   :background_id => 3
+  },
+  {:description => "When faced with a choice between money and my friends, I usually choose the money.",
+   :background_id => 3
+  },
+  {:description => "If there's a plan, I'll forget it. If I don't forget it, I'll ignore it.",
+   :background_id => 3
+  },
+  {:description => "I have a 'tell' that reveals when I'm lying.",
+   :background_id => 3
+  },
+  {:description => "I turn tail and run when things look bad.",
+   :background_id => 3
+  },
+  {:description => "An innocent person is in prison for a crime that I committed. I'm okay with that.",
+   :background_id => 3
+  },
+  {:description => "I know a story relevant to almost every situation.",
+   :background_id => 4
+  },
+  {:description => "Whenever I come to a new place, I collect local rumors and spread gossip.",
+   :background_id => 4
+  },
+  {:description => "I'm a hopeless romantic, always searching for that 'special someone.'",
+   :background_id => 4
+  },
+  {:description => "Nobody stays angry at me or around me for long, since I can defuse any amount of tension.",
+   :background_id => 4
+  },
+  {:description => "I love a good insult, even one directed at me.",
+   :background_id => 4
+  },
+  {:description => "I get bitter if I'm not the center of attention.",
+   :background_id => 4
+  },
+  {:description => "I'll settle for nothing less than perfection.",
+   :background_id => 4
+  },
+  {:description => "I change my mood or my mind as quickly as I change key in a song.",
+   :background_id => 4
+  },
+  {:description => "I judge people by their actions, not their words.",
+   :background_id => 5
+  },
+  {:description => "If someone is in trouble, I'm always ready to lend help.",
+   :background_id => 5
+  },
+  {:description => "When I set my mind to something, I follow through no matter what gets in my way.",
+   :background_id => 5
+  },
+  {:description => "I have a strong sense of fair play and always try to find the most equitable solution to arguments.",
+   :background_id => 5
+  },
+  {:description => "I'm confident in my own abilities and do what I can to instill confidence in others.",
+   :background_id => 5
+  },
+  {:description => "Thinking if for other people. I prefer action.",
+   :background_id => 5
+  },
+  {:description => "I misuse long words in an attempt to sound smarter.",
+   :background_id => 5
+  },
+  {:description => "I get bored easily. When am I going to get on with my destiny?",
+   :background_id => 5
+  },
+  {:description => "I believe that anything worth doing is worth doing right. I can't help it - I'm a perfectionist.",
+   :background_id => 7
+  },
+  {:description => "I'm a snob who looks down on those who can't appreciate fine art.",
+   :background_id => 7
+  },
+  {:description => "I always want to know how things work and what makes people tick.",
+   :background_id => 7
+  },
+  {:description => "I'm full of witty aphorisms and have a proverb for every occasion.",
+   :background_id => 7
+  },
+  {:description => "I'm rude to people who lack my commitment to hard work and fair play.",
+   :background_id => 7
+  },
+  {:description => "I like to talk at length about my profession.",
+   :background_id => 7
+  },
+  {:description => "I don't part with my money easily and will haggle tirelessly to get the best deal possible.",
+   :background_id => 7
+  },
+  {:description => "I'm well known for my work, and I want to make sure everyone appreciates it. I'm always take aback when people haven't heard of me.",
+   :background_id => 7
+  },
+  {:description => "I've been isolated for so long that I rarely speak, preferring gestures and the occasional grunt.",
+   :background_id => 6
+  },
+  {:description => "I am utterly serene, even in the face of disaster.",
+   :background_id => 6
+  },
+  {:description => "The leader of my community had something wise to say on every topic, and I am eager to share that wisdom.",
+   :background_id => 6
+  },
+  {:description => "I feel tremendous empathy for all who suffer.",
+   :background_id => 6
+  },
+  {:description => "I'm oblivious to etiquette and social expectations.",
+   :background_id => 6
+  },
+  {:description => "I connect everything that happens to me to a grand, cosmic plan.",
+   :background_id => 6
+  },
+  {:description => "I often get lost in my own thoughts and contemplation, becoming oblivious to my surroundings.",
+   :background_id => 6
+  },
+  {:description => "I am working on a grand philosophical theory and love sharing my ideas.",
+   :background_id => 6
+  },
+  {:description => "My eloquent flattery makes everyone I talk to feel like the most wonderful and important person in the world.",
+   :background_id => 8
+  },
+  {:description => "The common folk love me for my kindness and generosity.",
+   :background_id => 8
+  },
+  {:description => "No one could doubt by looking at my regal bearing that I am a cut above the unwashed masses.",
+   :background_id => 8
+  },
+  {:description => "I take great pains to always look my best and follow the latest fashions.",
+   :background_id => 8
+  },
+  {:description => "I don't like to get my hands dirty, and I won't be caught dead in unsuitable accommodations.",
+   :background_id => 8
+  },
+  {:description => "Despite my noble birth, I do not place myself above other folk. We all have the same blood.",
+   :background_id => 8
+  },
+  {:description => "My favor, once lost, is forever lost.",
+   :background_id => 8
+  },
+  {:description => "If you do my an injury, I will crush you, ruin your name, and salt your fields.",
+   :background_id => 8
+  },
+  {:description => "I'm driven by a wanderlust that led my away from home.",
+   :background_id => 9
+  },
+  {:description => "I watch over my friends as if they were a litter of newborn pups.",
+   :background_id => 9
+  },
+  {:description => "I once ran twenty-five miles without stopping to warn my clan of an approaching orc horde. I'd do it again if I had to.",
+   :background_id => 9
+  },
+  {:description => "I have a lesson for every siltation, drawn from observing nature.",
+   :background_id => 9
+  },
+  {:description => "I place no stock in wealthy or well-mannered folk. Money and manners won't save you from a hungry owlbear.",
+   :background_id => 9
+  },
+  {:description => "I'm always picking things up, absently fiddling with them, and sometimes accidentally breaking them.",
+   :background_id => 9
+  },
+  {:description => "I feel far more comfortable around animals than people.",
+   :background_id => 9
+  },
+  {:description => "I was, in fact, raised by wolves.",
+   :background_id => 9
+  },
+  {:description => "I use polysyllabic words that convey the impression of great education.",
+   :background_id => 10
+  },
+  {:description => "I've read every book in the world's greatest libraries - or I like to boast that I have.",
+   :background_id => 10
+  },
+  {:description => "I'm used to helping out those who aren't as smart as I am, and I patiently explain anything and everything to others.",
+   :background_id => 10
+  },
+  {:description => "There's nothing I like more than a good mystery.",
+   :background_id => 10
+  },
+  {:description => "I'm willing to listen to every side of an argument before I make my own judgment.",
+   :background_id => 10
+  },
+  {:description => "I ... speak ... slowly ... when talking ... to idiots, ... which ... almost ... everyone ... is ... compared ... to me.",
+   :background_id => 10
+  },
+  {:description => "I am horribly, horribly awkward in social situations.",
+   :background_id => 10
+  },
+  {:description => "I'm convinced that people are always trying to steal my secrets.",
+   :background_id => 10
+  },
+  {:description => "My friends know they can rely on me, no matter what.",
+   :background_id => 11
+  },
+  {:description => "I work hard so that I can play hard when the work is done.",
+   :background_id => 11
+  },
+  {:description => "I enjoy sailing into new ports and making new friends over a flagon of ale.",
+   :background_id => 11
+  },
+  {:description => "I stretch the truth for the sake of a good story.",
+   :background_id => 11
+  },
+  {:description => "To me, a tavern brawl is a nice way to get to know a new city.",
+   :background_id => 11
+  },
+  {:description => "I never pass up a friendly wager.",
+   :background_id => 11
+  },
+  {:description => "My language is as foul as an otyugh nest.",
+   :background_id => 11
+  },
+  {:description => "I like a job well done, especially if I can convince someone else to do it.",
+   :background_id => 11
+  },
+  {:description => "I'm always polite and respectful.",
+   :background_id => 12
+  },
+  {:description => "I'm haunted by memories of war. I can't get the images of violence out of my mind.",
+   :background_id => 12
+  },
+  {:description => "I've lost too many friends, and I'm slow to make new ones.",
+   :background_id => 12
+  },
+  {:description => "I'm full of inspiring and cautionary tales from my military experience relevant to almost every combat situation.",
+   :background_id => 12
+  },
+  {:description => "I can stare down a hell hound without flinching.",
+   :background_id => 12
+  },
+  {:description => "I enjoy being strong and like breaking things.",
+   :background_id => 12
+  },
+  {:description => "I have a crude sense of humor.",
+   :background_id => 12
+  },
+  {:description => "I face problems head-on. A simple, direct solution is the best path to success.",
+   :background_id => 12
+  },
+  {:description => "I hide scraps of food and trinkets away in my pockets.",
+   :background_id => 13
+  },
+  {:description => "I ask a lot of questions.",
+   :background_id => 13
+  },
+  {:description => "I like to squeeze into small places where no one else can get to me.",
+   :background_id => 13
+  },
+  {:description => "I sleep with my back to a wall or tree, with everything I own wrapped in a bundle in my arms.",
+   :background_id => 13
+  },
+  {:description => "I eat like a pig and have bad manners.",
+   :background_id => 13
+  },
+  {:description => "I think anyone who's nice to me is hiding evil intent.",
+   :background_id => 13
+  },
+  {:description => "I don't like to bathe.",
+   :background_id => 13
+  },
+  {:description => "I bluntly say what other people are hinting at or hiding.",
+   :background_id => 13
   }
 ])

9b7d02285ed81ef601a4bb43ceaff45ffe57d154
Jason Colyer <jcolyer2007@gmail.com>
2018-05-07 10:46:11 -0500

    Added all background bonds

--- db/seeds/12_bond.rb
+++ db/seeds/12_bond.rb
@@ -17,4 +17,220 @@ Bond.create([
   {:description => "I seek to preserve a sacred text that my enemies consider heretical and seek to destroy.",
    :background_id => 1
   },
+  {:description => "I fleeced the wrong person and must work to ensure that this individual never crosses paths with me or those I care about.",
+   :background_id => 2
+  },
+  {:description => "I owe everything to my mentor - a horrible person who's probably rotting in a jail somewhere.",
+   :background_id => 2
+  },
+  {:description => "Somewhere out there, I have a child who doesn't know me. I'm making the world better for him or her.",
+   :background_id => 2
+  },
+  {:description => "I come from a noble family, and one day I'll reclaim my lands and title from those who stole them from me.",
+   :background_id => 2
+  },
+  {:description => "A powerful person killed someone I love. Some day soon, I'll have my revenge.",
+   :background_id => 2
+  },
+  {:description => "I swindled and ruined a person who didn't deserve it. I seek to atone for my misdeeds but might never be able to forgive myself.",
+   :background_id => 2
+  },
+  {:description => "I'm trying to pay off an old debt I owe to a generous benefactor.",
+   :background_id => 3
+  },
+  {:description => "My ill-gotten gains go to support my family.",
+   :background_id => 3
+  },
+  {:description => "Something important was taken from me, and I aim to steal it back.",
+   :background_id => 3
+  },
+  {:description => "I will become the greatest thief that ever lived.",
+   :background_id => 3
+  },
+  {:description => "I'm guilty of a terrible crime. I hope I can redeem myself for it.",
+   :background_id => 3
+  },
+  {:description => "Someone I loved died because of a mistake I made. That will never happen again.",
+   :background_id => 3
+  },
+  {:description => "My instrument is my most treasured possession, and it reminds me of someone I love.",
+   :background_id => 4
+  },
+  {:description => "Someone stole my precious intrument, and someday I'll get it back.",
+   :background_id => 4
+  },
+  {:description => "I want to be famous, whatever it takes.",
+   :background_id => 4
+  },
+  {:description => "I idolize a hero of the old tales and measure my deeds against that person's.",
+   :background_id => 4
+  },
+  {:description => "I will do anything to prove myself superior to my hated rival.",
+   :background_id => 4
+  },
+  {:description => "I would do anything for the other members of my old troupe.",
+   :background_id => 4
+  },
+  {:description => "I have a family, but I have no idea where they are. One day, I hope to see them again.",
+   :background_id => 5
+  },
+  {:description => "I worked the land, I love the land, and I will protect the land.",
+   :background_id => 5
+  },
+  {:description => "A proud noble once gave me a horrible beating, and I will take my revenge on any bully I encounter.",
+   :background_id => 5
+  },
+  {:description => "My tools are symbols of my past life, and I carry them so that I will never forget my roots.",
+   :background_id => 5
+  },
+  {:description => "I protect those who cannot protect themselves.",
+   :background_id => 5
+  },
+  {:description => "I wish my childhood sweetheart had come with me to pursue my destiny.",
+   :background_id => 5
+  },
+  {:description => "The workshop where I learned my trade is the most important place in the world to me.",
+   :background_id => 7
+  },
+  {:description => "I created a great work for someone, and then found them unworthy to receive it. I'm still looking for someone worthy.",
+   :background_id => 7
+  },
+  {:description => "I owe my guild a great debt for forging me into the person I am today.",
+   :background_id => 7
+  },
+  {:description => "I pursue wealth to secure someone's love.",
+   :background_id => 7
+  },
+  {:description => "One day I will return to my guild and prove that I am the greatest artisan of them all.",
+   :background_id => 7
+  },
+  {:description => "I will get revenge on the evil forces that destroyed my place of business and ruined my livelihood.",
+   :background_id => 7
+  },
+  {:description => "Nothing is more important than the other members of my heritage, order, or association.",
+   :background_id => 6
+  },
+  {:description => "I entered seclusion to hide from the ones who might still be hunting me. I must someday confront them.",
+   :background_id => 6
+  },
+  {:description => "I'm still seeking the enlightenment I pursued in my seclusion, and it still eludes me.",
+   :background_id => 6
+  },
+  {:description => "I entered seclusion because I loved someone I could not have.",
+   :background_id => 6
+  },
+  {:description => "Should my discovery come to light, it could bring ruing to the world.",
+   :background_id => 6
+  },
+  {:description => "My isolation gave me great insight into a great evil that only I can destroy.",
+   :background_id => 6
+  },
+  {:description => "I will face any challenge to win the approval of my family.",
+   :background_id => 8
+  },
+  {:description => "My house's alliance with another noble family must be sustained at all costs.",
+   :background_id => 8
+  },
+  {:description => "Nothing is more important than the other members of my family.",
+   :background_id => 8
+  },
+  {:description => "I am in love with the heir of a family that my family despises.",
+   :background_id => 8
+  },
+  {:description => "My loyalty to my sovereign is unwavering.",
+   :background_id => 8
+  },
+  {:description => "The common folk must see me as a hero of the people.",
+   :background_id => 8
+  },
+  {:description => "My family, clan, or tribe is the most important thing in my life, even when they are far from me.",
+   :background_id => 9
+  },
+  {:description => "An injury to the unspoiled wilderness of my home is an injury to me.",
+   :background_id => 9
+  },
+  {:description => "I will bring terrible wrath down on the evildoers who destroyed my homeland.",
+   :background_id => 9
+  },
+  {:description => "I am the last of my tribe, and it is up to me to ensure their names enter legend.",
+   :background_id => 9
+  },
+  {:description => "I suffer awful visions of a coming disaster and will do anything to prevent it.",
+   :background_id => 9
+  },
+  {:description => "It is my duty to provide children to sustain my tribe.",
+   :background_id => 9
+  },
+  {:description => "It is my duty to protect my students.",
+   :background_id => 10
+  },
+  {:description => "I have an ancient text that holds terrible secrets that must not fall into the wrong hands.",
+   :background_id => 10
+  },
+  {:description => "I work to preserve a library, university, scriptorium, or monastery.",
+   :background_id => 10
+  },
+  {:description => "My life's work is a series of tomes related to a specific field of lore.",
+   :background_id => 10
+  },
+  {:description => "I've been searching my whole life for the answer to a certain question.",
+   :background_id => 10
+  },
+  {:description => "I sold my soul for knowledge. I hope to do great deeds and win it back.",
+   :background_id => 10
+  },
+  {:description => "I'm loyal to my captain first, everything else second.",
+   :background_id => 11
+  },
+  {:description => "The ship is most important - crewmates and captains come and go.",
+   :background_id => 11
+  },
+  {:description => "I'll always remember my first ship.",
+   :background_id => 11
+  },
+  {:description => "In a harbor town, I have a paramour whose eyes nearly stole me from the sea.",
+   :background_id => 11
+  },
+  {:description => "I was cheated out of my fair share of the profits, and I want to get my due.",
+   :background_id => 11
+  },
+  {:description => "Ruthless pirates murdered my captain and crewmates, plundered our ship, and left me to die. Vengeance will be mine.",
+   :background_id => 11
+  },
+  {:description => "I would still lay down my life for the people I served with.",
+   :background_id => 12
+  },
+  {:description => "Someone saved my life on the battlefield. To this day, I will never leave a friend behind.",
+   :background_id => 12
+  },
+  {:description => "My honor is my life.",
+   :background_id => 12
+  },
+  {:description => "I'll never forget the crushing defeat my company suffered or the enemies who dealt it.",
+   :background_id => 12
+  },
+  {:description => "Those who fight beside me are those worth dying for.",
+   :background_id => 12
+  },
+  {:description => "I fight for those who cannot fight for themsevles.",
+   :background_id => 12
+  },
+  {:description => "My town or city is my home, and I'll fight to defend it.",
+   :background_id => 13
+  },
+  {:description => "I sponsor an orphanage to keep others from enduring what I was forced to endure",
+   :background_id => 13
+  },
+  {:description => "I owe my survival to another urchin who taught me to live on the streets.",
+   :background_id => 13
+  },
+  {:description => "I owe a debt I can never repay to the person who took pity on me.",
+   :background_id => 13
+  },
+  {:description => "I escaped my life of poverty by robbing an important person, and I'm wanted for it.",
+   :background_id => 13
+  },
+  {:description => "No one else should have to endure the hardships I've been through.",
+   :background_id => 13
+  }
 ])

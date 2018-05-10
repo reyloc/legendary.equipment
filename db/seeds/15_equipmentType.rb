dff8e9b96eced0e666fc25c49f5e7a241152c610
Jason Colyer <jcolyer2007@gmail.com>
2018-05-10 12:59:21 -0500

    Added weapons

--- db/seeds/15_equipmentType.rb
+++ db/seeds/15_equipmentType.rb
@@ -16,5 +16,8 @@ EquipmentType.create([
   },
   {:name => 'Shield',
    :description => "A shield is made from wood or metal and is carried in one hand. Wielding a shield increases your Armor Class by 2. You can benefit from only one shield at a time."
+  },
+  {:name => 'Martial Weapons',
+   :description => 'Weapons that require more specialized training to be used effectively.'
   }
 ])

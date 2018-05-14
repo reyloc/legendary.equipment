65863ae0bcc395aaf2898d8413e8c0a7c777208d
Jason Colyer <jcolyer2007@gmail.com>
2018-05-14 10:26:38 -0500

    Added Monk features/levels/paths

--- db/seeds/22_classPath.rb
+++ db/seeds/22_classPath.rb
@@ -157,5 +157,23 @@ ClassPath.create([
    :feats => [134,135,136,137,138,139],
    :table => {},
    :char_class_id => 5
+  },
+  {:name => 'Way of the Open Hand',
+   :description => "Monks of the Way of the Open Hand are the ultimate masters of martial arts combat, whether armed or unarmed. They learn techniques to push and trip their opponents, manipulate ki to heal damage to their bodies, and practice advanced meditation that can protect them from harm.",
+   :feats => [169,170,171,172],
+   :table => {},
+   :char_class_id => 6
+  },
+  {:name => 'Way of the Shadow',
+   :description => "Monks of the Way of Shadow follow a tradition that values slealth and subterfuge. These monks might be called ninjas or shadowdancers, and they serve as spies and assassins. Sometimes the members of a ninja monastery are family members, forming a clan sworn to secrecy about their arts and missions. Other monasteries are more like thieves' guilds, hiring out their services to nobles, rich merchants, or anyone else who can pay their fees. Regardless of their methods, the heads of these monasteries expect the unquestioning obedience of their students.",
+   :feats => [173,174,175,176],
+   :table => {},
+   :char_class_id => 6
+  },
+  {:name => 'Way of the Four Elements',
+   :description => "You follow a monastic tradition that teaches you to harness the elements. When you focus your ki, you can align yourself with the forces of creation and bend the four elements to your will, using them as an extension of your body, Some members of this tradition dedicate themselves to a single element, but others weave the elements together. Many monks of this tradition tattoo their bodies with representations of their ki powers, commonly imagined as coiling dragons, but also as phoenixes, fish, plants, mountains, and cresting waves.",
+   :feats => [177,178],
+   :table => {},
+   :char_class_id => 6
   }
 ])
